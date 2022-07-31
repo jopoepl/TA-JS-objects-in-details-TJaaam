f5692660cc0c88a8a83e31047195ab73a7880710
jopoepl <joel_joseph93@outlook.com>
2022-07-31 17:02:18 +0530

    More About Class-updated

--- block-BJaahy/code/index.js
+++ block-BJaahy/code/index.js
@@ -7,7 +7,7 @@ class Square {
     }
 
     description(){
-        alert(`The square is ${width} x ${height}`)
+        alert(`The square is ${this.width} x ${this.height}`)
     }
 
     calcArea(){
@@ -23,8 +23,44 @@ class Square {
         this.height = Math.sqrt(value);
     }
 
-    static isEqual(square){
-        
+    static isEqual(a, b){
+        return (a.width * a.height) === (b.width * b.height)
 
     }
+
+    static isEqual(a, b){
+        return (a.width * a.height) === (b.width * b.height)
+    }
 }
+
+class User {
+    constructor(fname, lname){
+        this.firstname = fname;
+        this.lastname = lname;
+    }
+
+    get fullName(){
+        return this.firstname + this.lastname
+    }
+
+    set fullName(fullname){
+        if(fullname.length <= 5) {
+            alert(`full name can't be less than 5 charac`)
+        } else {
+        let fullNameArr = fullname.split(" ")
+        this.firstname = fullNameArr[0];
+        this.lastname = fullNameArr[1];
+        }
+    }
+
+    nameContains(str){
+        return `${this.firstname} ${this.lastname}`.includes(str)
+    }
+}
+
+let user1 = new User(`Joel`, `Joseph`)
+let user2 = new User(`Rahul`, `Mandyal`)
+
+user1.fullName;
+console.log(user1.nameContains(`oe`)) 
+
